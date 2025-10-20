13647772d82b9e22e376516bce815d1c95c959aa
Ronit <butironit@gmail.com>
2025-10-20 14:35:10 -0700

    feat(mail): add CourseAccessRequestMail class for handling course access requests

---
+++ app/Mail/CourseAccessRequestMail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Course;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class CourseAccessRequestMail extends Mailable
+{
+    use Queueable;
+    use SerializesModels;
+
+    public function __construct(
+        public readonly Course $course,
+        public readonly User $requester,
+        public readonly string $accessType,
+        public readonly ?string $requestMessage
+    ) {
+    }
+
+    public function build(): static
+    {
+        return $this->subject('Access request: '.$this->course->course_code.' '.$this->course->course_num.' - '.$this->course->course_title)
+            ->markdown('emails.courseAccessRequest', [
+                'course' => $this->course,
+                'requester' => $this->requester,
+                'accessType' => $this->accessType,
+                'requestMessage' => $this->requestMessage,
+            ]);
+    }
+}
+
+
