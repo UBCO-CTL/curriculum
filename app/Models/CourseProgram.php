6d691ce35645855bb04fbc9509fac04f014493e0
Ronit <butironit@gmail.com>
2025-10-30 14:24:16 -0700

    feat(course-clone): add ProcessCourseClone job for handling course cloning requests

--- app/Models/CourseProgram.php
+++ app/Models/CourseProgram.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CourseProgram extends Model
 {
     use HasFactory;
 
     protected $fillable = ['course_id', 'program_id', 'course_required', 'instructor_assigned', 'map_status'];
+
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class, 'course_id', 'course_id');
+    }
+
+    public function program(): BelongsTo
+    {
+        return $this->belongsTo(Program::class, 'program_id', 'program_id');
+    }
 }
