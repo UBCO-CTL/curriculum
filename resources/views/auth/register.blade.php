7e642a87b709b990d52bedd5e590bc66dab1346d
Natalie Kyei-Badu <nataliebaduu@gmail.com>
2026-07-05 10:51:15 -0700

    Edited register button color, made global variables in app.scss for frequent colors

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="register-view">
 <div class="container" style="padding-bottom:60px;">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -98,6 +98,7 @@
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
 
